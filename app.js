9534906a423592a04bccee37a25288f6153cf38e
Anna S <bamguanat@yandex.ru>
2019-10-05 16:32:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 const express = require('express');
+const data = require('./data.json') //require data.json
+const { projects } = data;
 
 const app = express();
 
-app.get();
+app.set('view engine', 'pug');
 
-app.listen(3000);
+app.use('/static', express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index', {projects}); 
+});
+
+app.get('/project:id', (req, res) => {
+    const { id } = req.params;
+    const { technologies } = projects[id];
+    res.render('project', {
+        project: projects[id], 
+        technologies
+    }); 
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.use((req, res, next) => {
+    const err = new Error ('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+  });
+
+app.listen(3000, () => {
+    console.log('The application is running at localhost:3000');
+});
