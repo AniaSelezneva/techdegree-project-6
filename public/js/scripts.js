186673f254a2e9ff468a15dda07756522164ab4f
Anna S <bamguanat@yandex.ru>
2019-10-05 19:57:20 +0300

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,11 +5,22 @@ const anchors = document.querySelectorAll('a');
 
 for( i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener ('mouseover', (target) => {
+        if (event.target.className !== 'button expanded') {
         event.target.style.transform = 'scale(1.07)';
         event.target.style.fontSize = '15px';
+        } else {
+            event.target.style.backgroundColor = 'grey';
+        }
     })
     anchors[i].addEventListener ('mouseout', (target) => {
-        event.target.style.transform = 'scale(1)';
-        event.target.style.fontSize = '13px';
+        if (event.target.className !== 'button expanded') {
+            event.target.style.transform = 'scale(1)';
+            event.target.style.fontSize = '13px';
+            } else {
+                event.target.style.backgroundColor = 'black';
+            }
     })
+    
 }
+
+
