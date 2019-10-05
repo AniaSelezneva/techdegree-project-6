c3bcd431ea609bc4267596bcb0d88d9e31f80426
Anna S <bamguanat@yandex.ru>
2019-10-05 19:08:56 +0300

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,15 @@
 $(document).foundation()
+
+
+const anchors = document.querySelectorAll('a');
+
+for( i = 0; i < anchors.length; i++) {
+    anchors[i].addEventListener ('mouseover', (target) => {
+        event.target.style.transform = 'scale(1.07)';
+        event.target.style.fontSize = '15px';
+    })
+    anchors[i].addEventListener ('mouseout', (target) => {
+        event.target.style.transform = 'scale(1)';
+        event.target.style.fontSize = '13px';
+    })
+}
